50b14bd5325dced6d3b3da2a67e772811d56869f
tschannes <mail@tscherrig.net>
2015-01-19 19:05:30 +0100

    found and fixed javascript incompatibility firefox chrome

--- js/fixed.js
+++ js/fixed.js
@@ -1,54 +1,51 @@
-function ad() {
-	window.addEventListener("scroll", function(evt) {
+bigScreenLimit = 1200;
 
-		// This value is your scroll distance from the top
-		var distance_from_top = document.body.scrollTop;
-		var ad = document.getElementsByClassName('ad')[0];
-
-		// The user has scrolled to the tippy top of the page. Set appropriate style.
-		if (distance_from_top < CheckAdHeight()) {
-			ad.removeAttribute("style");
-		}
-
-		// The user has scrolled down the page.
-		if (distance_from_top > CheckAdHeight()) {
-			ad.style.position = "fixed";
-			ad.style.top = "5em";
-			ad.style.right = (window.innerWidth) - 1200 /2;
-		}
-	});
-
-	function CheckAdHeight() {
+function makeAdSticky() {
+	function checkAdHeight() {
 		var distance = "";
 		if (document.getElementsByClassName('intro').length > 0) {
 			distance = document.getElementsByClassName('intro')[0].offsetHeight + 20;
 			return distance;
-		}else{
+		} else {
 			distance = 0;
 			return distance;
 		}
 	}
-}
 
-function checkWidth() {
-	var w = window.innerWidth;
-	if (w > 1200) {
-		return true;
-	}
+	window.addEventListener("scroll", function(evt) {
+
+		// This value is your scroll distance from the top
+		var distance_from_top = (document.documentElement.scrollTop || document.body.scrollTop);
+		var ad = document.getElementsByClassName('ad')[0];
+
+
+		// The user has scrolled to the tippy top of the page. Set appropriate style.
+		if (distance_from_top < checkAdHeight()) {
+			ad.removeAttribute("style");
+		}
+
+		// The user has scrolled down the page.
+		if (distance_from_top > checkAdHeight()) {
+			ad.style.position = "fixed";
+			ad.style.top = "5em";
+			ad.style.right = (window.innerWidth) - bigScreenLimit / 2;
+		}
+	});
 }
 
 function watchWidth() {
+	var w = window.innerWidth;
+	if (w > bigScreenLimit) {
+		makeAdSticky();
+	}
 	window.addEventListener("resize", function(evt) {
-		if (checkWidth() === true) {
-			ad();
-		}else{
+		if (w > bigScreenLimit) {
+			makeAdSticky();
+		} else {
 			location.reload();
 		}
 	});
 }
 
 /*Calling functions*/
-if (checkWidth() === true) {
-	ad();
-}
 watchWidth();
