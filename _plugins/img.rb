0d8443eb434eee197d02615dd27e605fb2d55acf
tschannes <mail@tscherrig.net>
2015-04-22 00:13:04 +0200

    first draft of responsive images done

---
+++ _plugins/img.rb
@@ -0,0 +1,15 @@
+require 'rubygems'
+require 'mini_magick'
+
+#variables 
+images = "./images/*/*.jpg"
+small_images = "./images/small/"
+
+Dir.glob(images) do |img|
+  next if img == '.' or img == '..'
+  	name = File.basename(img)
+  	image = MiniMagick::Image.open(img)
+	image.resize "386x168"
+	image.format "jpg"
+	image.write (small_images + name)
+end
