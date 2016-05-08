9e2f40d1931acb5334ab2cae1ca364553e0075e7
tschannes <mail@tscherrig.net>
2016-05-08 22:33:13 +0200

    started preparing for 12Z and 00Z

--- _assets/javascripts/dateGenerator.js
+++ _assets/javascripts/dateGenerator.js
@@ -1,3 +1,10 @@
+function imageExists(image_url) {
+    var http = new XMLHttpRequest();
+    http.open('HEAD', image_url, false);
+    http.send();
+    return http.status != 404;
+}
+
 function insertEmagramm() {
     var d = new Date();
     var year = d.getFullYear().toString();
@@ -7,12 +14,23 @@ function insertEmagramm() {
     day = ('0' + day).slice(-2);
     var dateString = year + month + day;
     /*console.log("dateString is: " + dateString);*/
-    var ema1 = "<img src='/loader.gif' data-layzr='http://www.meteoswiss.admin.ch/product/input/radio-soundings/VSSG76.LSSW_";
-    var ema2 = "_1200.gif'>";
-    var emaURL = ema1 + dateString + ema2;
-    /*console.log("emaURL is: " + emaURL);*/
-    document.getElementById("emagramm").innerHTML = emaURL;
+    var ema1 = "<img src='/loader.gif' data-layzr='";
+    var ema2 = "http://www.meteoswiss.admin.ch/product/input/radio-soundings/VSSG76.LSSW_";
+    var midday = "_1200.gif'";
+    var ema3 = ">";
+    var midnight = "_0000.gif'";
+    var emaDay = ema1 + ema2 + dateString + midday + ema3;
+    var emaNight = ema1 + ema2 + dateString + midnight + ema3;
+    var emaDayURL = ema2 + dateString + midday + ema3;
+    console.log("emaDayURL is: " + emaDayURL);
+    console.log("emaDay is: " + emaDay);
+    if (true /*imageExists(emaDayURL)*/) {
+        document.getElementById("emagramm").innerHTML = emaDay;
+    } else {
+        document.getElementById("emagramm").innerHTML = emaNight;
+    }
 }
+
 insertEmagramm();
 var layzr = new Layzr();
 layzr.update();
